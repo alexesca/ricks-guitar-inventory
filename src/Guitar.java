6bde4f108d85be93f184112b4358a69596a85ad8
alexesca <alexescamore@gmail.com>
2021-04-10 14:29:35 -0600

    Refactored guitar and added mandolin

--- src/Guitar.java
+++ src/Guitar.java
@@ -1,32 +1,7 @@
-import enums.*;
-
-public class Guitar
+public class Guitar extends Instrument
 {
-    private GuitarSpec spec;
-    private String serialNumber;
-    private double price;
-
     public Guitar(String serialNumber, double price, GuitarSpec spec)
     {
-        this.serialNumber = serialNumber;
-        this.price = price;
-        this.spec = spec;
-    }
-
-	public String getSerialNumber()
-    {
-        return serialNumber;
-    }
-
-    public double getPrice()
-    {
-        return price;
-    }
-    public void setPrice(float newPrice)
-    {
-        this.price = newPrice;
-    }
-    public GuitarSpec getSpec() {
-        return spec;
+        super(serialNumber, price, spec);
     }
 }
---
+++ src/Mandolin.java
@@ -0,0 +1,7 @@
+public class Mandolin extends Instrument
+{
+    public Mandolin(String serialNumber, double price, GuitarSpec spec)
+    {
+        super(serialNumber, price, spec);
+    }
+}
