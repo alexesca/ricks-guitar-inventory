51591078fd4f3ffeea6481d1d2f0c380b07bb2d6
alexesca <alexescamore@gmail.com>
2021-04-10 14:26:59 -0600

    Added mandolin spec concrete class

---
+++ src/MandolinSpec.java
@@ -0,0 +1,28 @@
+import enums.*;
+
+public class MandolinSpec extends InstrumentSpec {
+    private Style style;
+
+    public MandolinSpec(
+            Builder builder, String model, Type type,
+            Wood backWood, Wood topWood, Style style
+    ) {
+        super(builder, model, type, backWood,topWood);
+        this.style = style;
+    }
+
+    public Style getStyle() {
+        return this.style;
+    }
+
+    public Boolean matches(InstrumentSpec otherSpec) {
+        if(!super.matches(otherSpec))
+            return false;
+        if (!(otherSpec instanceof MandolinSpec))
+            return false;
+        MandolinSpec spec = (MandolinSpec) otherSpec;
+        if (style.equals(spec.style))
+            return false;
+        return true;
+    }
+}
