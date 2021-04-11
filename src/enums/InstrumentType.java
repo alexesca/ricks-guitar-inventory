34d4f43e14fdcf122335202b90e506969643b163
alexesca <alexescamore@gmail.com>
2021-04-10 20:26:48 -0600

    Added instrument type

---
+++ src/enums/InstrumentType.java
@@ -0,0 +1,17 @@
+package enums;
+
+public enum InstrumentType {
+    GUITAR, BANJO, DOBRO, FIDDLE, BASS, MANDOLIN;
+
+    public String toString() {
+        switch(this) {
+            case GUITAR: return "Guitar";
+            case BANJO: return "Guitar";
+            case DOBRO: return "Guitar";
+            case FIDDLE: return "Guitar";
+            case BASS: return "Guitar";
+            case MANDOLIN: return "Guitar";
+            default: return "Unspecified";
+        }
+    }
+}
